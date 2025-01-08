5b02dd144e85e692987b03b749653c8608f3cf20
adamsocrat  <ycs@min2s.com>
2025-01-08 13:44:55 +0100

    Backend is added

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+/**
+ * Starts the server and listens on the specified port.
+ * @function
+ * @name startServer
+ * @param {number} port - The port number to listen on.
+ * @returns {void}
+ */
+const port = 3000;
+const app = require('./src/app');
+
+app.listen(port, () => console.log(`app listening on port ${port}`));
