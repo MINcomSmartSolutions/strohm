7767bf555ebe9800899ec02891a116773263f01a
adamsocrat  <ycs@min2s.com>
2025-05-20 20:49:54 +0200

    unnecessary frontend removed

--- frontend/lib/common/services/navigator_key.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-
-/// A global key for accessing the navigator state of the app.
-final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
--- frontend/lib/common/utils/utils.dart
+++
@@ -1,5 +0,0 @@
-Map<String, dynamic> transformResultData(Map<String, dynamic> data, Map<String, dynamic> defaultValues) {
-  return defaultValues.map((key, defaultValue) {
-    return MapEntry(key, data[key] ?? defaultValue);
-  });
-}
